13ffbb0dcf222d1be48ace169655c8268c9d93f6
nvrmvx <zhanibek.n.karaganda@gmail.com>
2022-04-24 17:55:04 +0600

    feat: add traffic intensity, revenue functionality, refactor code

--- single_class.py
+++ single_class.py
@@ -3,35 +3,37 @@ import pandas as pd
 import random
 
 from simpy import Environment
-from simpy.rt import RealtimeEnvironment
 from simpy import Resource
 from simpy.events import Event
 
-BATT_DEG_A = 0.004
-BATT_DEG_B = 0.075
-BATT_DEG_C = 0.003
+PEV_NUM = 500
+LAM = 10.0
+S = 7
+R = 3
+SOC_RS = [0.60, 0.65, 0.70, 0.75, 0.80,0.85,0.90,0.95,0.99]
 
-#! the reward might be SoC_r-SoC_i instead of just SoC_r
-REWARD_M = 9 #! the paper says that it is calculated from price (but it is not)
-REWARD_N = 1
+BATT_DEG = {"a": 0.004,"b": 0.075,"c": 0.003}
+
+REWARD = {"m": 9,"n": 1}
 C_W = 20
 
-SOC_I_MU = 0.3
-SOC_I_SIGMA = 0.15
+SOC_I_P = {"mu": 0.3,"sigma": 0.15}
 P_MAX = 45
 E_MAX = 15
 E_C = 5
 
+T_CH_COEFFICIENT = 2
+
 class Pev:
-    def __init__(self, soc_i_mu, soc_i_sigma, p_max, e_max, e_c, soc_r, i, sim: 'Simulation'):
+    def __init__(self, soc_r, i, sim: 'Simulation'):
         self.i = i
-        soc_i = random.gauss(soc_i_mu,soc_i_sigma)
+        soc_i = random.gauss(sim.soc_i_mu,sim.soc_i_sigma)
         soc_i = max(0.05, min(soc_r-0.1, soc_i))
-        self.e_i = soc_i*e_max
-        self.e_c = e_c
-        self.e_r = soc_r*e_max
-        self.e_max = e_max
-        self.p_max = p_max
+        self.e_i = soc_i*sim.e_max
+        self.e_c = sim.e_c
+        self.e_r = soc_r*sim.e_max
+        self.e_max = sim.e_max
+        self.p_max = sim.p_max
         self.sim = sim
         self.sim.temp_pevs.append(
             {
@@ -42,6 +44,7 @@ class Pev:
                 "start_time": None,
                 "departure_time": None,
                 "mean_power": None,
+                "c_batt": None,
                 "blocked": False
             }
         )
@@ -56,8 +59,11 @@ class Pev:
             t1 = (self.e_c-e_i)/self.p_max
             e_i = self.e_c
         t2 = log((m1-n1*e_i)/(m1-n1*self.e_r),10.0)/n1
-        self.sim.temp_pevs[self.i-1]["mean_power"] = (self.p_max*t1+(2*m1-n1*(self.e_r+e_i))*t2/2.0)/(t1+t2)
-        return (t1+t2)*60.0
+        p_ow = (self.p_max*t1+(2*m1-n1*(self.e_r+e_i))*t2/2.0)/(t1+t2)
+        self.sim.temp_pevs[self.i-1]["mean_power"] = p_ow
+        self.sim.temp_pevs[self.i-1]["c_batt"] = self.sim.batt_deg["a"]*p_ow**2+self.sim.batt_deg["b"]*p_ow+self.sim.batt_deg["c"]
+        #! only multiplying by 2 gives the graphs from the paper
+        return (t1+t2)*60.0*self.sim.t_ch_coefficient
     
     def go_to_charging_station(self, env, charging_station: 'ChargingStation'):
         # at this point the PEV in question has just pulled up to the charging station
@@ -78,6 +84,8 @@ class Pev:
             self.sim.temp_pevs[self.i-1]["blocked"] = True
         # at this point the PEV in question is charged and is leaving the charging station
         self.sim.temp_pevs[self.i-1]["departure_time"] = env.now
+        if not (charging_station.admission or charging_station.charger.count):
+            self.sim.stop_event.succeed()
 
 class ChargingStation:
     def __init__(self, env, s, r):
@@ -87,6 +95,7 @@ class ChargingStation:
         for _ in range(s):
             self.charger_availability.append(True)
         self.waiting_space_capacity = r
+        self.admission = True
     
     def charge_pev(self, pev: Pev):
         for i in range(self.charger.capacity):
@@ -101,50 +110,48 @@ class ChargingStation:
 # this is the main class of the simulation
 # when it is initialized, the simulation is run automatically
 class Simulation:
-    def __init__(self,pev_num,lam,s,r,soc_rs,soc_i_mu=SOC_I_MU,soc_i_sigma=SOC_I_SIGMA,p_max=P_MAX,e_max=E_MAX,e_c=E_C):
+    def __init__(self,pev_num,lam,s,r,soc_rs,soc_i_p,p_max,e_max,e_c,batt_deg,reward,c_w,t_ch_coefficient):
         self.pev_num = pev_num
         self.lam = lam
         self.s = s
         self.r = r
-        self.soc_i_mu = soc_i_mu
-        self.soc_i_sigma = soc_i_sigma
+        self.soc_i_mu = soc_i_p["mu"]
+        self.soc_i_sigma = soc_i_p["sigma"]
         self.p_max = p_max
         self.e_max = e_max
         self.e_c = e_c
+        self.batt_deg = batt_deg
+        self.reward = reward
+        self.c_w = c_w
+        self.t_ch_coefficient = t_ch_coefficient
         self.soc_rs=soc_rs
         self.pevs = dict()
         for soc_r in self.soc_rs:
             self.env = Environment()
             self.temp_pevs = list()
             self.soc_r = soc_r
-            stop_event = Event(self.env)
-            self.env.process(self.run_charging_station(stop_event=stop_event))
-            self.env.run(stop_event)
+            self.stop_event = Event(self.env)
+            self.env.process(self.run_charging_station())
+            self.env.run(self.stop_event)
             self.temp_pevs = pd.DataFrame(self.temp_pevs)
             self.temp_pevs.set_index("pev", inplace = True)
             self.pevs[soc_r] = self.temp_pevs
 
     # process function for the simulation to run until a specified number of PEVs is charged
-    def run_charging_station(self, stop_event):
+    def run_charging_station(self):
         charging_station = ChargingStation(self.env, self.s, self.r)
-        admission = True
         i = 0
         while True:
             #TODO try a different way of doing this timeout (to mediate the discrepancies with the results from the paper)
             # wait time until next PEV has to be introduced to the simulation
             yield self.env.timeout(random.expovariate(self.lam/60))
             i += 1
-            if admission:
+            if charging_station.admission:
                 # create a new PEV in the simulation and send it to the charging station
-                pev = Pev(self.soc_i_mu, self.soc_i_sigma, self.p_max, self.e_max, self.e_c, self.soc_r, i, self)
+                pev = Pev(self.soc_r, i, self)
                 self.env.process(pev.go_to_charging_station(self.env,charging_station))
             if i >= self.pev_num:
-                # wait until all PEVs that are smaller than or equal to pev_num are charged and stop simulation
-                admission = False
-                if charging_station.charger.count == 0:
-                    #! this is assuming that all the charging is done in 120 minutes
-                    yield self.env.timeout(120)
-                    stop_event.succeed()
+                charging_station.admission = False
     
     def get_mean_charging_time(self):
         #TODO add calculation results too
@@ -164,8 +171,11 @@ class Simulation:
     
     def get_traffic_intensity(self):
         #TODO add calculation results too
-        #TODO finish this
-        return
+        temp = dict()
+        mu_over_1 = self.get_mean_charging_time()
+        for soc_r in self.soc_rs:
+            temp[soc_r] = mu_over_1[soc_r]*self.lam/(60*self.s)
+        return temp
     
     def get_blocking_probability(self):
         #TODO add calculation results too
@@ -184,26 +194,40 @@ class Simulation:
     
     def get_system_revenue(self):
         #TODO add calculation results too
-        #TODO finish this
-        return
+        temp1 = dict()
+        reward = lambda x: self.reward["m"]*x+self.reward["n"]
+        p_k = self.get_blocking_probability()
+        mean_t_w = self.get_mean_waiting_time()
+        mean_t_ch = self.get_mean_charging_time()
+        for soc_r in self.soc_rs:
+            temp2 = self.pevs[soc_r][self.pevs[soc_r]["blocked"]==False]
+            mean_c_batt = temp2["c_batt"].mean()
+            temp1[soc_r] = self.lam*(1-p_k[soc_r])*(reward(soc_r)-self.c_w*mean_t_w[soc_r]/60.0-mean_c_batt*mean_t_ch[soc_r]/60.0)
+        return temp1
 
     def get_results(self):
         return self.pevs
 
 if __name__ == "__main__":
-    #? env = RealtimeEnvironment(factor=0.1,strict=False)
     sim = Simulation(
-        pev_num=500,
-        lam=10,
-        s=7,
-        r=3,
-        soc_rs=[0.60, 0.65, 0.70, 0.75, 0.80,0.85,0.90,0.95,0.99]
+        pev_num=PEV_NUM,
+        lam=LAM,
+        s=S,
+        r=R,
+        soc_rs=SOC_RS,
+        soc_i_p=SOC_I_P,
+        p_max=P_MAX,
+        e_max=E_MAX,
+        e_c=E_C,
+        batt_deg=BATT_DEG,
+        reward=REWARD,
+        c_w=C_W,
+        t_ch_coefficient=T_CH_COEFFICIENT
     )
     print(sim.get_results())
     print(sim.get_mean_charging_time())
     print(sim.get_mean_charging_power())
-    print(sim.get_traffic_intensity()) #! does not work yet
+    print(sim.get_traffic_intensity())
     print(sim.get_blocking_probability())
     print(sim.get_mean_waiting_time())
-    print(sim.get_system_revenue()) #! does not work yet
-    #TODO add a graphing algorithm to compare it with the paper's results
+    print(sim.get_system_revenue())
